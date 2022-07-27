8bc1dceebdf8eeeccd6390d75d8a9fed0ef83a7e
RobPasMue <roberto.pastormuela@ansys.com>
2022-07-27 16:00:08 +0200

    Build C# files

---
+++ buildCSharp.sh
@@ -0,0 +1,10 @@
+# Bash script for generating auto-generated CSharp files from proto files
+#
+# First, clean the output directory
+rm -rf build/gen
+
+# Then, create the output directory
+mkdir -p build/gen
+
+# Finally, compile the proto files
+protoc --proto_path=. --csharp_out=build/gen --csharp_opt=file_extension=.g.cs,base_namespace ansys/api/discovery/v*/**.proto
