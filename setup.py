b8ba966d9d3c014e83c10cbab500929467a03e6f
Roberto Pastor Muela <37798125+RobPasMue@users.noreply.github.com>
2025-11-12 09:35:38 +0100

    build: bump ansys-api-geometry (#76)
    * build: bump ansys-api-geometry
    
    * Update pyproject.toml

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ if __name__ == "__main__":
         url=f"https://github.com/ansys/{package_name}",
         license="MIT",
         python_requires=">=3.10",
-        install_requires=["grpcio~=1.44", "protobuf>=3.19,<7", "ansys-api-geometry==0.4.85"],
+        install_requires=["grpcio~=1.44", "protobuf>=3.19,<7", "ansys-api-geometry==0.4.86"],
         packages=setuptools.find_namespace_packages(".", include=("ansys.*",)),
         package_data={
             "": ["*.proto", "*.pyi", "py.typed", "VERSION"],
