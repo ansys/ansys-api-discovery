c0c20ffb8cd0652bc34db9c1b4b1a4f4e986be94
Roberto Pastor Muela <37798125+RobPasMue@users.noreply.github.com>
2026-04-28 17:40:47 +0200

    fix: protobuf version (#124)

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ if __name__ == "__main__":
         url=f"https://github.com/ansys/{package_name}",
         license="MIT",
         python_requires=">=3.10",
-        install_requires=["grpcio~=1.44", "protobuf>=3.19", "ansys-api-geometry==0.4.92"],
+        install_requires=["grpcio~=1.44", "protobuf>=5.29", "ansys-api-geometry==0.4.92"],
         packages=setuptools.find_namespace_packages(".", include=("ansys.*",)),
         package_data={
             "": ["*.proto", "*.pyi", "py.typed", "VERSION"],
